60af01b3c25c20e7d7e81733e6f5da067e8a86e4
Vinda29 <vindahandayani29@students.amikom.ac.id>
2025-05-11 12:33:40 +0700

    Developer 2 changes

---
+++ CI-CD/main.py
@@ -0,0 +1,1 @@
+print("User A update")
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello from original")
+
+<<<<<<< HEAD
+print("Hello from Developer 1")
+=======
+print("Hello from Developer 2")
+>>>>>>> aecfe4d0c58596cbfa29d7ebc85e128a9d5c44c9
